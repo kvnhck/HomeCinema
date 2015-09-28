2dc2f6cd8e4061e9aa72f198b50905e069aac62a
Kevin Van Haecke <kevin@van-haecke.be>
2015-09-28 19:49:02 +0200

    Initial commit

---
+++ HomeCinema.Entities/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace HomeCinema.Entities
+{
+    public class Genre : IEntityBase
+    {
+        public Genre()
+        {
+            Movies = new List<Movie>();
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
